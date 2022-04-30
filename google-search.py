fbf454a91b0621e7e496a2b1a69183961a8fefbd
VictorRayan <victorrayansouzaramos@gmail.com>
2022-04-30 17:26:13 -0300

    Added automated search method with some filtered data from dataframe

--- google-search.py
+++ google-search.py
@@ -1,5 +1,6 @@
+import pandas as pd
+
 import os   #Importa biblioteca de sistema operacional para setar uma variável de ambiente com o caminho do webdriver (chromedrive)
-import time #Importa biblioteca respinsáve por criar Delay e Manipular operações de tempo
 
 #Importa bibliotecas necessárias do Selenium para simular interação humana entre as páginas.
 from selenium import webdriver
@@ -7,9 +8,41 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
 
 
+
+# Show all columns in pandas dataframe when printing to stdout
+pd.set_option("display.max_columns", None)
+
+datafile='/home/rayan/Documents/scientific_python/magnetic-dimensionality-v100.csv'
+df0 = pd.read_csv(datafile, ';')
+
+#print(df0.keys())
+
+# Drop all the columns except these ones
+df = df0[['icsd_code','dimension', 'chemical_structural_formula', 'num_atoms', 'space_group_symmetry', 'number_index', 'structure_type', 'valence', 'metal_neigh_coordination', 'partial_occupancy']].copy()
+
+# selected only materials with a given number of atoms, two-dimensional and no partial occupancy of atomic sites
+df_y = (df[ (df.num_atoms<=3) & (df.dimension==2) & (df.partial_occupancy=='no') ])
+
+#Lista com todos os termos do DataFrame que atenderam a condiçaõ estabelecida
+lista_de_materiais = list(df_y['chemical_structural_formula'])
+
+
+#Remove spaces
+lista_de_materiais=[ ''.join(m.split(' ')) for m in lista_de_materiais ] # Remove spaces
+
+print(lista_de_materiais)
+
+# List of words to be searched for
+lista_de_termos=['magnetic','metal','insulator']
+
+
+
+# Dictionary with the data gathered in the search
+gathered_data={'material':[],'search_word':[],'search_result':[]}
+
 #Método que carrega o browser, configura e retorna o objeto de 'driver' referente a interação no browser.
 def LoadBrowser():
-    executable_path = "/home/rayan/Documents/scientific_python/chromedrive"
+    executable_path = "./chromedrive"
     os.environ["webdriver.chrome.driver"] = executable_path
 
     chrome_options = Options()
@@ -17,30 +50,31 @@ def LoadBrowser():
 
     driver = webdriver.Chrome(executable_path=executable_path, chrome_options=chrome_options)
     driver.get("https://google.com")
-    time.sleep(1)
     return driver    
 
 
 
 #Método que faz a busca no google e obtem a quantidade de resultados através da varredura na estrutura DOM da página HTML de resultado
 #Pelo driver.
-def GoogleSearch(driver):
-	
+
+
+#REALIZA A BUSCA DOS TERMOS PASSADOS COMO PARAMETRO
+#parametro "driver" - inicaliza o navegador
+#parametro "term" - envia ao metodos os termos que serão pesquisados            
+def GoogleSearch(driver, term):
+	driver.get("https://google.com")
+
 	search_field = driver.find_element_by_xpath('/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input')
-	search_field.send_keys('Cristiano Ronaldo Aveiro dos Santos')
-	
+	search_field.send_keys(term)
 	search_field.send_keys(Keys.ENTER)
-	
-	time.sleep(1)
-	
 	results_google = driver.find_element_by_id('result-stats').text
-	print(results_google)
+	print(term+" "+results_google)
 
 
-
-#Executa os métodos.
+#BUSCA DOS DADOS DO DATAFRAME DE MATERIAIS E TERMOS NO GOOGLE
 driver = LoadBrowser()
-GoogleSearch(driver)
-
-	
-	
+for mat in lista_de_materiais:
+  for word in lista_de_termos:
+    gathered_data['material'].append(mat)
+    gathered_data['search_word'].append(word)
+    gathered_data['search_result'].append(GoogleSearch(driver, mat+" "+word))
